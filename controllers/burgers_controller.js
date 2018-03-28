6daec30269ffb8df91a66aea091d9c41bea52d72
Michael Neuman <mpneuman@Michaels-MBP.attlocal.net>
2018-03-27 23:18:08 -0400

    adds

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,49 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burgers.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res){
+	burger.all(function(data){
+		var hbsObject = {
+			burgers: data
+		};
+		console.log(hbsObject);
+		res.render("index", hbsObject);
+	});
+});
+
+router.post("/api/burgers", function(req, res) {
+	 burger.create([
+    "name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    condition: req.body.condition
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  cat.delete(condition, function() {
+    res.redirect("/");
+  });
+});
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("American Burger");
+INSERT INTO burgers (burger_name) VALUES ("Pizza Burger");
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
