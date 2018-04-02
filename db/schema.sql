976236970e308ad7e40d106c52560c758c20230c
Michael Neuman <mpneuman@wifi_stu-10-38-79-15.uncc.edu>
2018-04-02 18:31:10 -0400

    dba

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,10 @@
--- @author: Thomas Thompson
--- @github: tomtom28
--- @comment: Homework 14 - Eat the Burger!
 
-
-
--- Database Creation
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
 
--- Table Creation
+
 CREATE TABLE burgers(
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
 burger_name VARCHAR(50),
